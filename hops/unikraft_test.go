9ef5dbf6511c7e18be139d7d1cacd67567bc8053
Charalampos Mainas <cmainas@nubificus.co.uk>
2025-07-16 14:01:31 +0000

    Add more unit tests for the creation of rootfs in unikraft
    Add one more unit test to verify that the creation of a rootfs with an
    invalid format in the include files will fail.
    
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- hops/unikraft_test.go
+++ hops/unikraft_test.go
@@ -159,65 +159,84 @@ func TestUnikraftSupportsArch(t *testing.T) {
 }
 
 func TestUnikraftCreateRootfs(t *testing.T) {
-	plat := Platform{
-		Version: "1.0",
-		Monitor: "foo",
-		Arch:    "bar",
-	}
-	rootfs := Rootfs{
-		From:     "foo",
-		Path:     "bar",
-		Type:     "initrd",
-		Includes: []string{"foo:bar"},
-	}
+	t.Run("Rootfs type initrd and single file", func(t *testing.T) {
+		plat := Platform{
+			Version: "1.0",
+			Monitor: "foo",
+			Arch:    "bar",
+		}
+		rootfs := Rootfs{
+			From:     "scratch",
+			Type:     "initrd",
+			Includes: []string{"foo:bar"},
+		}
 
-	unikraft := NewUnikraft(plat, rootfs)
-	state, err := unikraft.CreateRootfs("context")
-	require.NoError(t, err)
-	def, err := state.Marshal(context.TODO())
+		unikraft := NewUnikraft(plat, rootfs)
+		state, err := unikraft.CreateRootfs("context")
+		require.NoError(t, err)
+		def, err := state.Marshal(context.TODO())
 
-	require.NoError(t, err)
-	m, arr := parseDef(t, def.Def)
-	// We expect 7 output states: 2 for copy files and 4 for initrd and 1 for final
-	// output
-	require.Equal(t, 7, len(arr))
-	// The last one (final output) should just have a single input
-	last := arr[len(arr)-1]
-	require.Equal(t, 1, len(last.Inputs))
-	// which is the exec op of initrd
-	lastInputDgst := last.Inputs[0].Digest
-	require.Equal(t, m[lastInputDgst], arr[5])
-	e := arr[5]
-	exec := e.Op.(*pb.Op_Exec).Exec
-	require.Equal(t, 3, len(exec.Meta.Args))
-	// the exec should have three inputs
-	require.Equal(t, 3, len(e.Inputs))
-	// the last of which should be the state with the initrd content
-	// that we passed as argument
-	sDgst := e.Inputs[2].Digest
-	require.Equal(t, m[sDgst], arr[4])
-	c := arr[4]
-	lDgst := c.Inputs[0].Digest
-	require.Equal(t, m[lDgst], arr[3])
-	l := arr[3].Op.(*pb.Op_Source).Source
-	require.Equal(t, "local://context", l.Identifier)
-	cf := c.Op.(*pb.Op_File).File
-	require.Equal(t, 1, len(cf.Actions))
-	cp := cf.Actions[0].Action.(*pb.FileAction_Copy).Copy
-	require.Equal(t, "/foo", cp.Src)
-	require.Equal(t, "/bar", cp.Dest)
-	// the second of which should be the state that creates the directory
-	// where the cpio file will get stored
-	oDgst := e.Inputs[1].Digest
-	require.Equal(t, m[oDgst], arr[2])
-	// the first of which should be the state that creates the tmp directory
-	tDgst := e.Inputs[0].Digest
-	require.Equal(t, m[tDgst], arr[1])
-	tmp := arr[1]
-	// The state that create the tmp directory has one input, which is the source of
-	// the tools
-	toolDgst := tmp.Inputs[0].Digest
-	require.Equal(t, m[toolDgst], arr[0])
+		require.NoError(t, err)
+		m, arr := parseDef(t, def.Def)
+		// We expect 7 output states: 2 for copy files and 4 for initrd and 1 for final
+		// output
+		require.Equal(t, 7, len(arr))
+		// The last one (final output) should just have a single input
+		last := arr[len(arr)-1]
+		require.Equal(t, 1, len(last.Inputs))
+		// which is the exec op of initrd
+		lastInputDgst := last.Inputs[0].Digest
+		require.Equal(t, m[lastInputDgst], arr[5])
+		e := arr[5]
+		exec := e.Op.(*pb.Op_Exec).Exec
+		require.Equal(t, 3, len(exec.Meta.Args))
+		// the exec should have three inputs
+		require.Equal(t, 3, len(e.Inputs))
+		// the last of which should be the state with the initrd content
+		// that we passed as argument
+		sDgst := e.Inputs[2].Digest
+		require.Equal(t, m[sDgst], arr[4])
+		c := arr[4]
+		lDgst := c.Inputs[0].Digest
+		require.Equal(t, m[lDgst], arr[3])
+		l := arr[3].Op.(*pb.Op_Source).Source
+		require.Equal(t, "local://context", l.Identifier)
+		cf := c.Op.(*pb.Op_File).File
+		require.Equal(t, 1, len(cf.Actions))
+		cp := cf.Actions[0].Action.(*pb.FileAction_Copy).Copy
+		require.Equal(t, "/foo", cp.Src)
+		require.Equal(t, "/bar", cp.Dest)
+		// the second of which should be the state that creates the directory
+		// where the cpio file will get stored
+		oDgst := e.Inputs[1].Digest
+		require.Equal(t, m[oDgst], arr[2])
+		// the first of which should be the state that creates the tmp directory
+		tDgst := e.Inputs[0].Digest
+		require.Equal(t, m[tDgst], arr[1])
+		tmp := arr[1]
+		// The state that create the tmp directory has one input, which is the source of
+		// the tools
+		toolDgst := tmp.Inputs[0].Digest
+		require.Equal(t, m[toolDgst], arr[0])
+	})
+	t.Run("Invalid files structure", func(t *testing.T) {
+		plat := Platform{
+			Version: "1.0",
+			Monitor: "foo",
+			Arch:    "bar",
+		}
+		rootfs := Rootfs{
+			From:     "foo",
+			Path:     "bar",
+			Type:     "initrd",
+			Includes: []string{":bar", "ka"},
+		}
+
+		unikraft := NewUnikraft(plat, rootfs)
+		_, err := unikraft.CreateRootfs("context")
+		require.Error(t, err)
+		require.ErrorContains(t, err, "Invalid format of the file")
+	})
 }
 
 func TestUnikraftBuildKernel(t *testing.T) {
